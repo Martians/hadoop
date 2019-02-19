4c3ba8be81844cb8aa66a54c94864ae0ba3c5b5c
liudong <liudong@daowoo.com>
2019-02-19 14:46:15 +0800

    fix

--- flink/src/main/scala/com/data/Source.scala
+++
@@ -1,9 +0,0 @@
-package com.data
-
-object Source {
-    val WORDS = Array(
-      "To be, or not to be,--that is the question:--",
-      "Whether 'tis nobler in the mind to suffer",
-      "The slings and arrows of outrageous fortune"
-    )
-}
